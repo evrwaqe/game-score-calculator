daf722b7ce688348b8a138acd3be073a688963a8
evrwaqe <evrwaqe@gmail.com>
2025-05-24 17:46:24 -0300

    docs(api): add auth to swagger

--- apps/api/src/infra/docs/swagger.ts
+++ apps/api/src/infra/docs/swagger.ts
@@ -6,11 +6,22 @@ const setupSwagger = (app: INestApplication): void => {
   const config = new DocumentBuilder()
     .setTitle('Game Score Calculator API')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build()
   const doc = SwaggerModule.createDocument(app, config)
   const theme = new SwaggerTheme()
   const options = {
     customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    persistAuthorization: true,
   }
   SwaggerModule.setup('docs', app, doc, options)
 }
--- apps/api/src/modules/calculations/controllers/calculations.controller.ts
+++ apps/api/src/modules/calculations/controllers/calculations.controller.ts
@@ -3,7 +3,10 @@ import { AuthGuard } from '@nestjs/passport'
 import { User } from '@prisma/client'
 import { AddCalculationService } from '../services'
 import { AddCalculationRequestDTO } from '../dtos/request'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Calculations')
+@ApiBearerAuth('access-token')
 @Controller('calculations')
 export class CalculationsController {
   constructor(private readonly addCalculationService: AddCalculationService) {}
