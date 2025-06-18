c5bb48d9ceccd21f4343fa5c86191163ae326c3b
evrwaqe <evrwaqe@gmail.com>
2025-06-17 22:19:34 -0300

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { FaUser } from 'react-icons/fa'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div className="flex justify-between items-center">
+      <div className="w-5"></div>
+      <h1 className="text-main-color text-2xl font-bold text-center">
+        Game Score Calculator
+      </h1>
+      <FaUser size={20} className="mt-1 cursor-pointer" />
+    </div>
+  )
 }
