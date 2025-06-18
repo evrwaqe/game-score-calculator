0e8c2a040fcbc59d8e25d70fbf1795673b133c18
evrwaqe <evrwaqe@gmail.com>
2025-06-18 00:50:23 -0300

    feat: add link from header to profile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { FaUser } from 'react-icons/fa'
 
@@ -8,7 +9,9 @@ export function Header() {
       <h1 className="text-main-color text-2xl font-bold text-center">
         Game Score Calculator
       </h1>
-      <FaUser size={20} className="mt-1 cursor-pointer" />
+      <Link href="/profile" aria-label="Go to profile page">
+        <FaUser size={20} className="mt-1 cursor-pointer" />
+      </Link>
     </div>
   )
 }
