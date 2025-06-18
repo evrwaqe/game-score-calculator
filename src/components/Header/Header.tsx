caaab40a88c338291982dd91d5f3f02f7ff341b0
evrwaqe <evrwaqe@gmail.com>
2025-06-18 00:51:37 -0300

    feat: add link from header to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,11 @@ export function Header() {
   return (
     <div className="flex justify-between items-center">
       <div className="w-5"></div>
-      <h1 className="text-main-color text-2xl font-bold text-center">
-        Game Score Calculator
-      </h1>
+      <Link href="/">
+        <h1 className="text-main-color text-2xl font-bold text-center">
+          Game Score Calculator
+        </h1>
+      </Link>
       <Link href="/profile" aria-label="Go to profile page">
         <FaUser size={20} className="mt-1 cursor-pointer" />
       </Link>
