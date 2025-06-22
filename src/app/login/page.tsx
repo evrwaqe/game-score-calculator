170e948d8c20293f1e0c195efa0ebfd73315e45f
evrwaqe <evrwaqe@gmail.com>
2025-06-21 23:17:56 -0300

    feat: add base style and layout to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Login() {
-  return <div>Login</div>
+  return (
+    <div className="flex flex-col border border-main-color rounded-[0.25rem] mt-20 p-3 gap-8 w-full max-w-[30rem] items-center">
+      <div>Profile image</div>
+      <div>Login with your Google account</div>
+      <div>Login with Google Button</div>
+    </div>
+  )
 }
 
 export default Login
